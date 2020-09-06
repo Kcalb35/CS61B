1c035abadc3bbfa7c86cc12e2661de49601966f6
kcalb35 <1356hengblack@gmail.com>
2020-09-06 16:32:26 +0800

    completed lab2 truely

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,7 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         if(A==null){
-            A.rest = B;
+            A = new IntList(B.first,B.rest);
             return A;
         }
         else{
